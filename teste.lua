d300bcff5010c8efb49375cd3eef13af094bd902
Wilson Júnior <wilsonpjunior@gmail.com>
2025-01-31 14:16:51 -0300

    Remove undesired code

--- teste.lua
+++
@@ -1,4 +0,0 @@
-package.path = package.path .. ";/Users/wilson/.luarocks/share/lua/5.1/?.lua";
-package.cpath = package.cpath .. ";/Users/wilson/.luarocks/lib/lua/5.1/?.so";
-
-local jwt = require('resty.jwt');
